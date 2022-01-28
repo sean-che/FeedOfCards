f0db725d0aac7567d264718ca4fe519ece6cdf55
sean-che <sergii.chekotylo@gmail.com>
2022-01-28 01:03:31 -0500

    Create a Service

--- src/main/java/com/example/feedofcards/CardsController.java
+++ src/main/java/com/example/feedofcards/CardsController.java
@@ -2,25 +2,25 @@ package com.example.feedofcards;
 
 import com.example.feedofcards.model.DailyCard;
 import com.example.feedofcards.model.StatusCard;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CardsController {
+
+    private final CardsService cardsService;
 
     @GetMapping("/dailyCard")
     public ResponseEntity<DailyCard> getDailyCard() {
-        return ResponseEntity.ok(new DailyCard("Daily Quote", "quote1", "author1"));
+        return ResponseEntity.ok(cardsService.getDailyCard());
     }
 
     @GetMapping("/statusCard")
     public ResponseEntity<StatusCard> getStatusCard() {
-        return ResponseEntity.ok(new StatusCard(
-                "Important Security Update",
-                "https://tinyurl.com/y5mdph2g",
-                "For your security, we support 2 factor authentication, and would recommend that you turn it on.",
-                "Got it, thanks"));
+        return ResponseEntity.ok(cardsService.getStatusCard());
     }
 
 }
--- src/main/java/com/example/feedofcards/CardsService.java
+++ src/main/java/com/example/feedofcards/CardsService.java
@@ -1,6 +1,7 @@
 package com.example.feedofcards;
 
 import com.example.feedofcards.model.DailyCard;
+import com.example.feedofcards.model.StatusCard;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,15 @@ import org.springframework.stereotype.Service;
 public class CardsService {
 
     public DailyCard getDailyCard() {
-        
+        return new DailyCard("Daily Quote", "quote1", "author1");
+    }
+
+    public StatusCard getStatusCard() {
+        return new StatusCard(
+                "Important Security Update",
+                "https://tinyurl.com/y5mdph2g",
+                "For your security, we support 2 factor authentication, and would recommend that you turn it on.",
+                "Got it, thanks");
     }
 
 }
